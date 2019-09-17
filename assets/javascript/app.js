0fff03b6bdb904ca050d8eeae7f4d5491bf911ae
Tom Cox <thomasxcox731@gmail.com>
2019-09-17 18:03:52 -0500

    Added the pseudocode for the javascript

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+//create array of strings
+var topics = [""];
+
+//create function to create buttons in html 
+
+//Use for loop to append a button for each string in an array 
+
+//When the button is clicked, page gets 10 static gif's from the api and puts it on the page
+
+//When the user clicks the gif, it animates. Clicked again, becomes static
+
+//Display ratings under gifs
+
+//Add form that takes a value from a user input box and adds it to the topics array 
