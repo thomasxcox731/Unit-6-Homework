13e613084105bb8ded8c3766b50e1225556f5714
Tom Cox <thomasxcox731@gmail.com>
2019-09-19 09:50:31 -0500

    Added details to the for loop in the javascript.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,49 @@
+//Link to API with query URL 
+
+//Use ajax (url: queryURL, method: "GET")
+
 //create array of strings
 var topics = ["dogs", "cats", "bears"];
 
 //create function to create buttons in html, and give id of "button"
-function buttons(){
-    $("<button>").html(topics).attr("id", "button");
+// function buttons(){
+//     $("<button>").html(topics).attr("id", "button");
     
 }
 //Use for loop to append a button for each string in an array 
 for (i = 0; i < topics.length; i++){
-    $("#button").append(topics); //add specific object to topics
+    var a = $("<button>");
+    a.addClass("button");
+    a.attr("data-name", topics[i]);
+    a.text(topics[i]);
+    $(".button").append(a);
     console.log(topics);
 }
 //When the button is clicked,
 $("#button").on("click", function(){
     //page gets 10 static gif's from the api and puts it on the page
+    //Link to API with query URL 
+    var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=9kQ9GZBwVwfnaxFBovjF05JRmaYvZpAi&tag=" + topics;
+    $.ajax ({
+        url: queryURL,
+        method: "GET",
+    })
+    .then(function(response){
+         // Saving the image_original_url property
+         var imageUrl = response.data.image_original_url;
+
+         // Creating and storing an image tag
+         var animalImage = $("<img>");
+          // Setting the catImage src attribute to imageUrl
+          animalImage.attr("src", imageUrl);
+          animalImage.attr("alt", "animal image");
+           // Prepending the animalImage to the images div
+           $("#images").prepend(animalImage);
+    })
+
 })
+
+
 //When the user clicks the gif, it animates. Clicked again, becomes static
 
 //Display ratings under gifs
@@ -26,4 +55,5 @@ function newSubmit() {
             topics.push("#submit id")
         })
     })
+    buttons();
 }
