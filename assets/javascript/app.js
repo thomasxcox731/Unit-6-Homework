bb47eb743fad893f6c2f1261883b834931e3ed31
Tom Cox <thomasxcox731@gmail.com>
2019-09-19 14:57:52 -0500

    Gif's are now appearing

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,17 +5,13 @@
 //create array of strings
 var topics = ["Dogs", "Cats", "Bears"];
 
-//create function to create buttons in html, and give id of "button"
-// function buttons(){
-//     $("<button>").html(topics).attr("id", "button");
-    
-
 //Use for loop to append a button for each string in an array 
 function renderButtons() {
     $("#button").empty();
     for (i = 0; i < topics.length; i++) {
         var a = $("<button>");
         a.attr("id","button");
+        a.addClass("topics")
         a.attr("data-name", topics[i]);
         a.text(topics[i]);
         $("#button").append(a);
@@ -23,18 +19,20 @@ function renderButtons() {
     }
 }
 //When the button is clicked,
-$("#button").on("click", function () {
+$(document).on("click",".topics", function () {
     //page gets 10 static gif's from the api and puts it on the page
     //Link to API with query URL 
-    var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=9kQ9GZBwVwfnaxFBovjF05JRmaYvZpAi&tag=" + topics;
+    var topicClicked = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=9kQ9GZBwVwfnaxFBovjF05JRmaYvZpAi&tag=" + topicClicked;
     $.ajax({
         url: queryURL,
         method: "GET",
     })
         .then(function (response) {
             // Saving the image_original_url property
+            console.log(response);
             var imageUrl = response.data.image_original_url;
-
+            console.log(imageUrl);
             // Creating and storing an image tag
             var animalImage = $("<img>");
             // Setting the catImage src attribute to imageUrl
