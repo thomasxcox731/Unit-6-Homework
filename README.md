cee42b94cff5a51484d3e4aec5b17b625a8fc943
Tom Cox <thomasxcox731@gmail.com>
2019-09-19 23:45:01 -0500

    Final commit prior to deadline.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Unit-6-Homework
-This is the website for Unit 6: AJAX
+This is the website for Unit 6: AJAX. On this website, the user will input the name of an animal and upon selecting it, it will generate ten gif's pertaining to the name of the animal. 
+
+Animal Apparitions: Table of Contents
+
+    -assets 
+
+        -css
+            -reset.css
+            -style.css
+        
+        -javascript
+            -app2.js
+   
+    -index.html
+
--- assets/javascript/app2.js
+++ assets/javascript/app2.js
@@ -36,48 +36,32 @@ $(document).on("click", ".topics", function () {
                     Image.attr("data-state", "still");
                     Image.addClass("gif");
                     //Adding info to HTML
-                    gifDiv.append(p);
                     gifDiv.append(Image);
+                    gifDiv.append(p);
                     $("#gifs-appear-here").prepend(gifDiv);
-                    $(".gif").on("click", function() {
-                        // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+                    //Adding the pausing functionality
+                    $(".gif").on("click", function () {
                         var state = Image.attr("data-state");
-                        // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-                        // Then, set the image's data-state to animate
-                        // Else set src to the data-still value
                         if (state === "still") {
-                          Image.attr("src", Image.attr("data-animate"));
-                          Image.attr("data-state", "animate");
+                            Image.attr("src", Image.attr("data-animate"));
+                            Image.attr("data-state", "animate");
                         } else {
-                          Image.attr("src", Image.attr("data-still"));
-                          Image.attr("data-state", "still");
+                            Image.attr("src", Image.attr("data-still"));
+                            Image.attr("data-state", "still");
                         }
-                      });
+
+                    })
+
                 }
             }
         })
+
 });
 //Adding new custom buttons
-$("#add-animal").on("click", function(event) {
+$("#add-animal").on("click", function (event) {
     event.preventDefault();
     var newBtn = $("#animal-input").val().trim();
     topics.push(newBtn);
     renderButtons();
 });
-//Adding pausing functionality to gifs
-
-// $(".gif").on("click", function() {
-//     // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
-//     var state = Image.attr("data-state");
-//     // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-//     // Then, set the image's data-state to animate
-//     // Else set src to the data-still value
-//     if (state === "still") {
-//       Image.attr("src", Image.attr("data-animate"));
-//       Image.attr("data-state", "animate");
-//     } else {
-//       Image.attr("src", Image.attr("data-still"));
-//       Image.attr("data-state", "still");
-//     }
-//   });
 renderButtons();
